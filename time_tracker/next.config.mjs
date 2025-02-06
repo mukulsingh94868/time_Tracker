a017fbb18fe9323195958d09117c45a1aaf39331
mukul65 <mukul.singh@zentekinfosoft.com>
2025-02-06 17:45:10 +0530

    modify

--- time_tracker/next.config.mjs
+++ time_tracker/next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
     reactStrictMode: true,
     trailingSlash: true, // Ensure pages generate with a trailing slash (optional)
     output: 'standalone', // Ensure Vercel serves it correctly
+    async redirects() {
+        return [
+            {
+                source: "/:path*",
+                destination: "/",
+                permanent: false,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
